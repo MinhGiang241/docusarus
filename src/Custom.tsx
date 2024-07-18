4b0516e6dd6c2df2ef94fee9e3681dbf841bb17c
Minh Giang <minhgiang241@gmail.com>
2024-07-18 11:30:11 +0700

    html

--- src/Custom.tsx
+++ src/Custom.tsx
@@ -1,14 +1,33 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 function Custom(props: any) {
   console.log("po", props);
+
+  const [html, setHtml] = useState<string | undefined>();
+  const getHtml = async () => {
+    try {
+      var res: any = await axios.post(
+        "https://api.tutorial.tmas.net.vn/apimodel/webcontent.getContent",
+        { href: props.data?.href },
+      );
+      if (res?.code != 0) {
+        return;
+      }
+      setHtml(res?.data?.data);
+    } catch (error) {}
+  };
+
+  useEffect(() => {
+    getHtml();
+  }, [props.data?.href]);
 
   return (
     <div>
       <h1>Custom</h1>
       <h4 style={{ color: "red" }}>{props.data.label}</h4>
       <p>{props.data.href}</p>
-      <div dangerouslySetInnerHTML={{ __html: props.data.html ?? "" }}></div>
+      <div dangerouslySetInnerHTML={{ __html: html ?? "" }}></div>
     </div>
   );
 }
